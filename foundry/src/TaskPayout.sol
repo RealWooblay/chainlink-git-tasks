2af361cc5133cccf5501ddbca71b32317550cbdc
ParanoidSyntaxError <git@jbojtor.com>
2024-05-22 09:25:53 +1000

    Added govenor

---
+++ foundry/src/TaskPayout.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+
+contract TaskPayout {
+
+    constructor() {
+
+    }
+
+    function merge() external payable {
+        // Call functions consumer - returns PR author
+
+        // Get token id from PR author's GitHub id
+
+        if(msg.value > 0) {
+            // Send PR author ETH
+        }
+    }
+}
