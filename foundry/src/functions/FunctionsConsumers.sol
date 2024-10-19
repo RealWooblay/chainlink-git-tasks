6032f324cc8276722f2c9a42bb532b61d1faa51b
ParanoidSyntaxError <git@jbojtor.com>
2024-10-20 04:11:14 +1100

    Fixed sol versions

--- foundry/src/functions/FunctionsConsumers.sol
+++ foundry/src/functions/FunctionsConsumers.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity ^0.8.25;
 
 import {IFunctionsSubscriptions} from "@chainlink/functions/v1_0_0/interfaces/IFunctionsSubscriptions.sol";
 
--- foundry/src/functions/GithubTasks.sol
+++ foundry/src/functions/GithubTasks.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity ^0.8.25;
 
 import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
 import {Address} from "@openzeppelin/utils/Address.sol";
